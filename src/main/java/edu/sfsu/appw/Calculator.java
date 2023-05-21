652233e56132d2c5cd19d2c8429acf67332d73d0
weatherpub <patrick@bayareamedia.net>
2023-05-20 17:19:56 -0700

    Refactored Calculator.java

--- src/main/java/edu/sfsu/appw/Calculator.java
+++ src/main/java/edu/sfsu/appw/Calculator.java
@@ -5,57 +5,61 @@ import java.awt.*;
 
 public class Calculator extends JFrame {
     public Calculator() {
-        System.out.println("Calculator");
+        System.out.println("Calculator Running...");
     }
-    static JPanel pPanel = new JPanel();
-    static JPanel fPanel = new JPanel();
-    static JPanel controlPanel = new JPanel();
+    void addComponentsToPane(Container pane) {
+        JLabel n_label;
+        JLabel iy_label;
+        JTextField n_textField;
+        JTextField iy_textField;
 
-    static JPanel buttonPanel = new JPanel();
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.fill = GridBagConstraints.HORIZONTAL;
+
+        n_label = new JLabel("N");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 2;
+        c.weightx = 0.0;
+        c.gridx = 0;
+        c.gridy = 1;
+        pane.add(n_label, c);
+
+        n_textField = new JTextField(10);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 1;
+        c.gridx = 2;
+        c.gridy = 1;
+        c.weightx = 0.0;
+        pane.add(n_textField, c);
+
+        iy_label = new JLabel("I/Y");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 2;
+        c.gridx = 0;
+        c.gridy = 2;
+        c.weightx = 0.0;
+        pane.add(iy_label, c);
+
+        iy_textField = new JTextField(10);
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 1;
+        c.gridx = 2;
+        c.gridy = 2;
+        c.weightx = 0.0;
+        pane.add(iy_textField, c);
+    }
     void createWindow() {
-        JButton jb = new JButton("Calculate");
-        JFrame frame = new JFrame("SFSU TVM Calculator");
+        JFrame frame = new JFrame("Financial Calculator");
+        JPanel panel = new JPanel();
 
-        frame.setSize(500, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.getContentPane().setLayout(new GridLayout(3,1));
+        frame.setSize(350, 400);
 
-        JTabbedPane tab = new JTabbedPane(JTabbedPane.TOP);
-        tab.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+        addComponentsToPane(frame.getContentPane());
 
-        tab.add("PV", pPanel); // present value tab
-        tab.add("FV", fPanel); // future value tab
-
-        // present value
-        pPanel.add(new JLabel("Present Value"));
-        pPanel.add(new JLabel("N"));
-        pPanel.add(new JTextField(10));
-
-        pPanel.add(new JLabel("I/Y"));
-        pPanel.add(new JTextField(10));
-
-        pPanel.add(new JLabel("PV"));
-        pPanel.add(new JTextField(10));
-
-        pPanel.add(new JLabel("PMT"));
-        pPanel.add(new JTextField(10));
-
-        // Future Value
-        // fPanel.add(new JLabel("Future Value"));
-        // fPanel.add(new JTextField(10));
-
-        add(jb);
-
-        controlPanel.setLayout(new GridLayout());
-
-       /* The calculate button will have two settings, one for PV and the other for FV.
-        * A flag will be set for equalsIgnoreCase(), for the two states of the button: PV, FV.
-        * The PV subclass will render the computation for the PV and FS as well.
-        * * */
-
-        frame.add(tab, BorderLayout.CENTER);
-        frame.add(controlPanel);
+        frame.add(panel);
         frame.setVisible(true);
     }
 }
